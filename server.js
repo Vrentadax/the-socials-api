bdaf9a735817184d50ab03575170f227a28090fd
Vrentadax <vrentadax@outlook.com>
2022-06-16 19:29:44 -0600

    created the user model

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+/* This is the server.js file. It is the entry point for the application. It is where we set up our
+express server and connect it to MongoDB using mongoose. It also requires the routes we created in
+routes/api.js. We define a PORT and set it to 3001. We'll use this later in our config file when we
+set up the proxy for the client. */
 const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
